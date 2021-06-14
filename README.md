8eacd41dd1399c7570a939407a67b637a0ed2a7b
Alex <aleksandr-stec@mail.ru>
2021-06-14 20:44:25 +0300

    first react-ts app 3 lesson

--- README.md
+++
@@ -1,1 +0,0 @@
-# innopolis_homework
---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App = () => {
+    return(
+        <h1>Hello world для проекта - innopolis</h1>
+    )
+}
+
+export default App;
+
